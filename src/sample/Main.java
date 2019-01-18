7508708ad0fe3c09a8c83a63ba01acc6e378fff7
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2019-01-18 12:07:08 +0100

    plot two point clouds and a line

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,9 @@
 package sample;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.ColorModel;
 import java.io.FileReader;
 import java.util.List;
 import javax.swing.*;
@@ -8,7 +11,8 @@ import com.opencsv.CSVReader;
 
 import org.jfree.chart.*;
 import org.jfree.chart.plot.*;
-import org.jfree.data.xy.DefaultXYDataset;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.*;
 
 public class Main {
 
@@ -20,7 +24,7 @@ public class Main {
         label.setBounds(160,150, 80,40);
         f.add(label);
 
-        f.setSize(400,400);
+        f.setSize(600,600);
         f.setLayout(null);
         f.setVisible(true);
 
@@ -32,11 +36,14 @@ public class Main {
             System.out.println("Error loading CSV: " + e.getLocalizedMessage());
         }
 
-        JFreeChart chart = createChart();
+        double[][] points1 = {{5.0,12.0},{15.0,20.0}};
+        double[][] points2 = {{20.0,8.0},{25.0,20.0}};
+        JFreeChart chart = createChart(1.0, 0.0, 50.0, points1, points2);
+
         ChartPanel panel = new ChartPanel(chart);
-        panel.setPreferredSize(new Dimension(400, 400));
+        panel.setPreferredSize(f.getSize());
         f.setContentPane(panel);
-        //SwingUtilities.updateComponentTreeUI(f);
+        SwingUtilities.updateComponentTreeUI(f);
     }
 
     private static double[][] parse(String filename) throws Exception {
@@ -59,23 +66,53 @@ public class Main {
         return Double.valueOf(parts[1]);
     }
 
-    private static JFreeChart createChart() {
-        DefaultXYDataset dataset = lineDataset(1.0, 0.0, 50.0);
-        return ChartFactory.createXYLineChart("TheChart", "X",
+    private static JFreeChart createChart(double a, double b, double maxX, double[][] points1, double[][] points2) {
+        XYSeries lineSeries = lineSeries(a, b, maxX);
+        XYSeries pointsSeries1 = pointSeries("PointSeries1", points1);
+        XYSeries pointsSeries2 = pointSeries("PointSeries2", points2);
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(lineSeries);
+        dataset.addSeries(pointsSeries1);
+        dataset.addSeries(pointsSeries2);
+
+        JFreeChart chart = ChartFactory.createXYLineChart("TheChart", "X",
                 "Y", dataset, PlotOrientation.VERTICAL, false, false, false);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesLinesVisible(0, true);
+        renderer.setSeriesShapesVisible(0, false);
+        renderer.setSeriesPaint(0, Color.black);
+        renderer.setSeriesLinesVisible(1, false);
+        renderer.setSeriesShapesVisible(1, true);
+        renderer.setSeriesPaint(1, Color.red);
+        Shape s = renderer.getSeriesShape(0);
+        renderer.setSeriesLinesVisible(2, false);
+        renderer.setSeriesShapesVisible(2, true);
+        renderer.setSeriesPaint(2, Color.blue);
+        plot.setRenderer(renderer);
+
+        return chart;
     }
 
-    private static DefaultXYDataset lineDataset(double a, double b, double maxX) {
+    private static XYSeries lineSeries(double a, double b, double maxX) {
         int res = 100;
-        double data[][] = new double[2][res];
+        XYSeries series = new XYSeries("LineSeries");
         double step = maxX / ((double) 100);
         for(int i = 0; i < res; i++) {
             double x = i * step;
-            data[0][i] = x;
-            data[1][i] = a * x + b;
+            double y = a * x + b;
+            series.add(x, y);
         }
-        DefaultXYDataset dataset = new DefaultXYDataset();
-        dataset.addSeries("TheLine", data);
-        return dataset;
+        return series;
+    }
+
+    private static XYSeries pointSeries(String key, double[][] points) {
+        XYSeries series = new XYSeries(key);
+        for(int i = 0; i < points.length; i++) {
+            series.add(points[i][0], points[i][1]);
+        }
+        return series;
     }
 }
