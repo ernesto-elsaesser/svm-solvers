1e143c3d9571a880a038bf48b929bcd8c9b5a530
Ernesto Elsäßer <ernesto.elsaesser@me.com>
2019-01-18 10:24:24 +0100

    implement csv parsing

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,7 @@
 package sample;
 
 import java.io.FileReader;
+import java.util.List;
 import javax.swing.*;
 import com.opencsv.CSVReader;
 
@@ -15,14 +16,30 @@ public class Main {
         f.setVisible(true);
 
         try {
-            loadData();
+            double[][] values = loadData();
+            // TODO: apply SVM
         } catch (Exception e) {
             System.out.println("Error loading CSV: " + e.getLocalizedMessage());
         }
     }
 
-    private static void loadData() throws Exception {
-        CSVReader reader = new CSVReader(new FileReader("data/svmguide1.csv"));
-        //TODO: read CSV
+    private static double[][] loadData() throws Exception {
+        CSVReader reader = new CSVReader(new FileReader("data/svmguide1.csv"), ' ');
+        List<String[]> lines = reader.readAll();
+        int num = lines.size();
+        double[][] values = new double[num][4];
+        for (int i = 0; i < num; i++) {
+            String[] line = lines.get(i);
+            values[i][0] = parseValue(line[1]);
+            values[i][1] = parseValue(line[2]);
+            values[i][2] = parseValue(line[3]);
+            values[i][3] = parseValue(line[4]);
+        }
+        return values;
+    }
+
+    private static double parseValue(String s) {
+        String[] parts = s.split(":");
+        return Double.valueOf(parts[1]);
     }
 }
