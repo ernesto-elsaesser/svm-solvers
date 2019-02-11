05f86b50f52e647861fd628a715bf9f36d17a4de
Markus Lippert <mail@lippertmarkus.de>
2019-02-11 15:14:37 +0100

    draw non-linear

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ import org.jfree.data.xy.*;
 
 public class Main {
 
-    private static int DATA_SET = 2;
+    private static int DATA_SET = 3;
     private static boolean USE_CACHED_PLANE = false;
     private static boolean USE_SMO = true;
 
@@ -79,7 +79,11 @@ public class Main {
             h = deriveHyperplane(originalVectors);
         }
 
-        JFreeChart chart = createChart(originalVectors, h);
+        JFreeChart chart;
+        if (svm.dataIsLinearilySeparatable)
+            chart = createChart(originalVectors, h);
+        else
+            chart = testcreateChart(originalVectors, svm);
         ChartPanel panel = new ChartPanel(chart);
         panel.setPreferredSize(f.getSize());
         f.setContentPane(panel);
@@ -142,6 +146,102 @@ public class Main {
     private static double parseValue(String s) {
         String[] parts = s.split(":");
         return Double.valueOf(parts[1]);
+    }
+
+    private static JFreeChart testcreateChart(List<SupportVector> vectors, SVM svm) {
+
+        XYSeries hyperplane   = areaSeries2(svm, "Hyperplane");
+        XYSeries class1points = pointSeries(vectors, (byte)0, "Class1Points");
+        XYSeries class2points = pointSeries(vectors, (byte)1,"Class2Points");
+
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+
+        dataset.addSeries(class1points);
+
+        dataset.addSeries(class2points);
+        dataset.addSeries(hyperplane);
+
+        JFreeChart chart = ChartFactory.createXYLineChart("SVMChart", "",
+                "", dataset, PlotOrientation.VERTICAL, false, false, false);
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+
+        renderer.setSeriesLinesVisible(1, false);
+        renderer.setSeriesShapesVisible(1, true);
+        renderer.setSeriesPaint(1, Color.red);
+
+        renderer.setSeriesLinesVisible(2, false);
+        renderer.setSeriesShapesVisible(2, true);
+        renderer.setSeriesPaint(2, Color.yellow);
+
+        renderer.setSeriesLinesVisible(0, false);
+        renderer.setSeriesShapesVisible(0, true);
+        renderer.setSeriesPaint(0, Color.blue);
+
+        plot.setRenderer(renderer);
+
+        return chart;
+    }
+
+    private static XYSeries areaSeries(SVM svm, String key) {
+        XYSeries series = new XYSeries(key);
+        for (int i=-100;i<100;i++) {
+            for (int j=-100;j<100;j++) {
+                int wert1 = i;
+                int wert2 = j;
+                double[] werte = {wert1, wert2};
+                double erg = svm.output(werte);
+                if(erg < 0){
+                    series.add(wert1, wert2);
+                }
+                double wert3 = i+0.5;
+                double wert4 = j+0.5;
+                double[] werte2 = {wert3, wert4};
+                erg = svm.output(werte2);
+                if(erg < 0){
+                    series.add(wert3, wert4);
+                }
+            }
+        }
+
+        return series;
+    }
+
+    private static XYSeries areaSeries2(SVM svm, String key) {
+        XYSeries series = new XYSeries(key);
+
+        double xMin = 0.0;
+        double xMax = 0.0;
+        double yMin = 0.0;
+        double yMax = 0.0;
+
+        for (SupportVector v : svm.vectors) {
+            if (v.x[0] > xMax) {
+                xMax = v.x[0];
+            }
+            if (v.x[1] > yMax) {
+                yMax = v.x[1];
+            }
+            if (v.x[0] < xMin) {
+                xMin = v.x[0];
+            }
+            if (v.x[1] < yMin) {
+                yMin = v.x[1];
+            }
+        }
+
+        for(double x = xMin-3; x < xMax+3; x+=0.5) {
+            for(double y = yMin-3; y < yMax+3; y+=0.5) {
+                double classification = svm.output(new double[] {x, y});
+
+                if (classification <= 0)
+                    series.add(x, y);
+            }
+        }
+
+        return series;
     }
 
     private static JFreeChart createChart(List<SupportVector> vectors, Hyperplane h) {
